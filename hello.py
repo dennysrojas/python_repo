1d6f7c64b7cbdfe736c788d34c100d0c0c75338c
Dennys Rojas <dennysrojas@hotmail.es>
2025-04-22 17:44:03 -0500

    feat: añadir script hello.py con ejemplos básicos de impresión y tipos de datos en Python

---
+++ hello.py
@@ -0,0 +1,23 @@
+#Este es un comentario: Hola Python 
+print("Hola Python")   
+print('Hola Python')
+
+
+"""
+Este es un 
+comentario 
+de varias lineas
+"""
+
+'''
+Este también es un 
+comentario 
+de varias lineas
+'''
+
+#Consultar el tipo de dato
+print(type("Hola Python")) #str   
+print(type(5)) #int
+print(type(1.5)) #float 
+print(type(True)) #bool
+print(type(3+1j)) #complex
